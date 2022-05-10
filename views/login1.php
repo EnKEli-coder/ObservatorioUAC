0ca81aba1c0e12e4cb8e57cf0913cc2d134a839e
omar romero <omar_erg@hotmail.com>
2022-05-10 18:57:38 -0500

    sistema de usuarios

--- views/login1.php
+++ views/login1.php
@@ -1,27 +1,48 @@
 <?php
+
+require './model/conexion.php';
+
+if(!empty($_POST['email']) && !empty($_POST['password'])){
+  $records = $conn->prepare('SELECT id, email, password FROM users WHERE email=:email');
+  $records->bindParam(':email', $_POST['email']);
+  $records->execute();
+  $results = $records->fetch(PDO::FETCH_ASSOC);
+
+  $message = '';
+
+  if(count($results)>0 && password_verify($_POST['password'],$results['password'])){
+    $_SESSION['user_id'] = $results['id'];
+    header('Location: ?menu=mapa');
+  }else{
+    $message ='Sorry, wrong credentials';
+  }
+}
 $estado_session = session_status();
 if($estado_session == PHP_SESSION_NONE)
 {
     session_start();
 }
 
-if (isset($_SESSION['loggedUserName'])) {
-?>
-    <h1>No tiene permisos para entrar a la página <h1>
-<?php    
-}else {    
 ?>
 
 <link rel="stylesheet" href="styles/login1.css">
+
+<?php
+    if(!empty($message)):
+?>
+    <p><?= $message ?></p>
+<?php
+    endif;
+?>
+
 <div class="log-form">
   <h2>Iniciar Sesion</h2>
-  <form action="./controller/login.php" method="post">
-    <input type="text" title="correo" id="email" placeholder="Correo" />
-    <input type="password" title="username" id="password" placeholder="Contraseña" />
+  <form action="?menu=login" method="post">
+    <input type="text" name="email" menu="email" title="correo" id="email" placeholder="Correo" />
+    <input type="password" name="password" menu="password" title="username" id="password" placeholder="Contraseña" />
     <button type="submit" class="btn">Ingresar</button>
     <a class="forgot" href="#">Olvido su usuario/Contraseña?</a>
   </form>
 </div>
 <?php
-}
 ?>
--- views/registro.php
+++ views/registro.php
@@ -1,11 +1,39 @@
-<?php?>
+<?php
+    require './model/conexion.php';
+
+    $message = '';
+
+    if(!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['name'])) {
+        $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':name',$_POST['name']);
+        $stmt->bindParam(':email',$_POST['email']);
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $stmt->bindParam(':password',$password);
+
+        if($stmt->execute()){
+            $message = 'Succesfully created new user';
+        }else{
+            $message = "Sorry there must be an issue";
+        }
+    }
+
+?>
 <link rel="stylesheet" href="styles/registro.css">
+<?php
+    if(!empty($message)):
+?>
+    <p><?= $message ?></p>
+<?php
+    endif;
+?>
+
 <div class="register-form">
     <h2>Registra tu cuenta</h2>
-    <form>
-        <input type="text" title="nombre" placeholder="Nombre" />
-        <input type="text" title="email" placeholder="Correo" />
-        <input type="password" title="username" placeholder="Contraseña" />
+    <form action="?menu=register" method='post'>
+        <input type="text" name ="name" title="nombre" placeholder="Nombre" />
+        <input type="text" name="email" title="email" placeholder="Correo" />
+        <input type="password" name="password" title="username" placeholder="Contraseña" />
         <button type="submit" class="btn">Registrarse</button>
     </form>
     </div><!--end log form -->   
